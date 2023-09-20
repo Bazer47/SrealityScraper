911e25ff73d7504eeac5478ba89bcfee497df90b
Bazer47 <honzabayer@seznam.cz>
2023-09-21 00:08:40 +0200

    Add views with html and css

--- views.py
+++ views.py
@@ -1,6 +1,19 @@
-from app import app
-from 
+from itertools import groupby
+from operator import attrgetter
 
-@app.route('/')
+from flask import render_template
+from sqlalchemy import select
+
+from app import app
+from database import db
+from models import Property, Image
+
+@app.route("/")
 def index():
-    return 'Hello World!'
+    properties = db.session.execute(select(Property).order_by(Property.id)).all()
+    properties = [prop[0] for prop in properties]
+    images = db.session.execute(select(Image).order_by(Image.id)).all()
+    images = [img[0] for img in images]
+    images_groupby = {k: list(g) for k, g in groupby(images, attrgetter('property_id'))}
+    # print(images_groupby)
+    return render_template("index.html", properties=properties, images=images_groupby)
