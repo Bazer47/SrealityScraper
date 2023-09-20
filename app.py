6035fa137d79e940eff6e6bd7a6ba8e18ea240a4
Bazer47 <honzabayer@seznam.cz>
2023-09-21 00:08:40 +0200

    Add db connection

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import os
 
 from flask import Flask
-import psycopg2
+from flask_migrate import Migrate
+from sqlalchemy import select
+
+from database import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
 
-import views
+from models import Property, Image
+from views import *
 
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='flask_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return conn
+with app.app_context():
+    db.create_all()
+
+    if len(db.session.execute(select(Property).order_by(Property.id)).all()) < 12:
+        prop_tmp = Property(title="Byt", title_url="Byt_url", description="Byt_descr")
+        db.session.add(prop_tmp)
+        db.session.flush()
+        prop_id_tmp = prop_tmp.id
+        print(f"{prop_id_tmp = }")
+        db.session.add(Image(property_id=prop_id_tmp, url="https://d18-a.sdn.cz/d_18/c_img_QJ_Jo/6e0BFt5.jpeg?fl=res,400,300,3|shr,,20|jpg,90"))
+        db.session.add(Image(property_id=prop_id_tmp, url="https://d18-a.sdn.cz/d_18/c_img_QJ_Jo/6e0BFt5.jpeg?fl=res,400,300,3|shr,,20|jpg,90"))
+        db.session.add(Image(property_id=prop_id_tmp, url="https://d18-a.sdn.cz/d_18/c_img_QJ_Jo/6e0BFt5.jpeg?fl=res,400,300,3|shr,,20|jpg,90"))
+        db.session.commit()
+    props = db.session.execute(select(Property).order_by(Property.id))
+    imgs = db.session.execute(select(Image).order_by(Image.id))
+    print(props.all())
+    print(imgs.all())
+    # db.drop_all()
+    # db.session.commit()
+
+if __name__ == "__main__":
+    app.run(debug=True)
