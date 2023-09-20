bfa4a5256042d3fa466e3c4e85dda5a7b802550f
Bazer47 <honzabayer@seznam.cz>
2023-09-21 00:08:40 +0200

    Delete spider 1

--- web_scrape.py
+++ web_scrape.py
@@ -2,65 +2,6 @@ import json
 
 import scrapy
 
-
-class SrealitySpider(scrapy.Spider):
-    name = 'sreality_spider'
-    N_OF_PAGES = 1
-    urls = ["https://www.sreality.cz/hledani/prodej/byty"]
-    # urls_pages = ["https://www.sreality.cz/hledani/prodej/byty?strana=" + str(i) for i in range(N_OF_PAGES)]
-    # urls = urls + urls_pages
-
-    # @classmethod
-    # def update_settings(cls, settings):
-    #     super().update_settings(settings)
-    #     settings.set(
-    #         "DOWNLOAD_HANDLERS",
-    #         {
-    #             "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    #             "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    #         },
-    #         priority="spider"
-    #     )
-    #     # settings.set(
-    #     #     "TWISTED_REACTOR",
-    #     #     # "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
-    #     #     "twisted.internet.epollreactor.EPollReactor",
-    #     #     priority="spider"
-    #     # )
-
-    def start_requests(self):
-        for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse, meta={"playwright": True})
-
-    def parse(self, response):
-        with open('page.html', 'wb') as html_file:
-            html_file.write(response.body)
-        N_OF_IMAGES = 3
-        PAGE_RANGE = 3
-        BASE_URL = "https://www.sreality.cz"
-        props = response.css('div.property')
-        for i_property in range(PAGE_RANGE):
-            prop = props[i_property]
-            print(f"{prop = }")
-            title = prop.css('span.name').get()
-            print(f"{title = }")
-            title_url = prop.css('a.title::attr(href)').get()
-            title_url = BASE_URL + title_url
-            price = prop.css('span.norm-price').get()
-            print(f"{price = }")
-            description = prop.css('span.locality::text').get()
-            print(f"{description = }")
-            imgs_urls = []
-            for i_img in range(N_OF_IMAGES):
-                imgs_urls.append(prop.css('img::attr(src)')[i_img].get())
-            yield {
-                "title": title,
-                "title_url": title_url,
-                "description": description,
-                "price": price,
-                "imgs_urls": imgs_urls
-            }
-
 class SrealitySpider2(scrapy.Spider):
     name = 'sreality_spider_2'
     BASE_URL = "https://www.sreality.cz"
@@ -109,22 +50,6 @@ class SrealitySpider2(scrapy.Spider):
                 "price": price,
                 "imgs_urls": imgs_urls
             }
-
-
-# crawler_process = CrawlerProcess(
-#     settings={
-#         "FEEDS": {
-#             "web_scrape.json": {"format": "json", 'encoding': 'utf8', 'indent': 4},
-#         },
-#         # "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
-#         # "DOWNLOAD_HANDLERS": {
-#         #     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-#         #     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-#         # },
-#         # "CONCURRENT_REQUESTS": 32
-#     }
-# )
-
 
 # if __name__ == "__main__":
 #     crawler_process.crawl(SrealitySpider2)
