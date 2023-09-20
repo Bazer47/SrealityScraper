89a88fae344bb1fe19ac07b54d994717b9d79b02
Bazer47 <honzabayer@seznam.cz>
2023-09-21 00:08:40 +0200

    Add price to property model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
 
@@ -9,10 +9,11 @@ class Property(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100)) 
     title_url: Mapped[str] = mapped_column(String(1000))
+    price: Mapped[str] = mapped_column(String(30))
     description: Mapped[str] = mapped_column(String(1000))
 
     def __repr__(self) -> str:
-        return f"Property(id={self.id}, title={self.title}, title_url={self.title_url}, description={self.description})"
+        return f"Property(id={self.id}, title={self.title}, title_url={self.title_url}, price={self.price}, description={self.description})"
     
 class Image(Base):
     __tablename__ = "image"
